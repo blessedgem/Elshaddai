36b8d4596682e5fb5f1c91d436ce07a5e1e7ab4a
jemima <emereck7@gmail.com>
2016-04-19 17:58:25 +0000

    Export Stuff

--- exportDatabase.sh
+++ exportDatabase.sh
@@ -1,1 +1,1 @@
-ssh cloudera@10.76.254.127 'sqoop import-all-tables --connect 'jdbc:mysql://10.76.254.50:3306/Sales' --username=root --password=gem --warehouse-dir=/user/hive/warehouse  --hive-import -m 1' &> exportDatabse.out
+ssh cloudera@10.76.254.127 'sqoop import-all-tables --connect 'jdbc:postgresql://10.76.254.50:5432/Project' --username=postgres --password=gem --warehouse-dir=/user/hive/warehouse  --hive-import -m 1' &> exportDatabse.out
