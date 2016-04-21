ee4ce016d174dfd326683af69706c38ffdbe9a9a
Nana Kwame <nanakwame123@gmail.com>
2016-04-21 16:20:34 +0000

    extending functionality

--- addJob.php
+++ addJob.php
@@ -1,24 +1,54 @@
 <?php
 require "vendor/autoload.php";
 require "ExportDatabase.php";
+//require "ImportTable.php";
+require 'database.php';
 
-$data = require "ajumamoro.conf.php";
+if($_POST['graph'])
+{
+    $reponse = Database::query($_REQUEST, $_POST);
+//    $job = new ImportTable();
+    
+    $atiaa = \ntentan\atiaa\Driver::getConnection(
+        array(
+            'driver' => 'postgresql',
+            'dbname' => 'dummy',
+            'password'=> $post['password'],
+            'user' => $post['username'],
+            'host'=> $post['host']
+        )
+    );
+    $atiaa->query("CREATE table dummy_table");
+    
+    foreach ($reponse['result'] as $data)
+    {
+        $value = 0;
+        $counter = 0;
+        foreach ($data as $value)
+        {
+            $values = $counter == '0' ? "" : $values . ", ";
+            $values .= $value;
+            $counter ++;
+        }
+        
+        echo json_encode($values);die();
+        $atiaa->query("CREATE table dummy_table");
+        $atiaa->query("INSERT INTO table_name ({$_POST['cols']}) VALUES ($values);");
+    }
+}
 
-$username =$_POST['username'];
-$databasename =$_POST['databasename'];
-$password =$_POST['password'];
-$localhost=$_POST['localhost'];
-$virtualhost=$_POST['virtualhost'];
-$drivername = $_POST['databasetype'];
-$dbport = $_POST['portnumber'];
-$dirname = $_POST['dirname'];
+else
+{
+    $data = require "ajumamoro.conf.php";
+    $job = new ExportDatabase();
+}
 
 $queue = ajumamoro\Queue::connectBroker([
     'driver' => 'redis',
     'scheme' => 'tcp',
     'host' => '127.0.0.1'
 ]);
-$job = new ExportDatabase();
+
 $job->addAttribute('username', $_POST['username']);
 $job->addAttribute('databasename',$_POST['databasename']);
 $job->addAttribute('password',$_POST['password']);
