93482c8e3bdbd852bf37fc805679342b4994d431
Nana Kwame <nanakwame123@gmail.com>
2016-04-21 16:21:46 +0000

    5000 records

--- database.php
+++ database.php
@@ -88,7 +88,7 @@ class Database
         $conditions .= $globalSearch . $columnSearch;
         
         return array(
-            "query" => "SELECT $columns FROM {$post['tablename']} $conditions $order LIMIT 500",
+            "query" => "SELECT $columns FROM {$post['tablename']} $conditions $order LIMIT 5000",
             "display_query" => "SELECT $columns FROM {$post['tablename']} $conditions $order $limit"
         );
     }
