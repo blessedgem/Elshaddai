734807b3702cc0168bb7c51906bbb5d9bb0287b0
Nana Kwame <nanakwame123@gmail.com>
2016-04-27 16:37:18 +0000

    testing process

--- createDatabase.php
+++ createDatabase.php
@@ -1,28 +1,28 @@
 <?php
 //
-//require "vendor/autoload.php";
+require "vendor/autoload.php";
+
+$username =$_POST['username'];
+$database =$_POST['databasename'];
+$password =$_POST['password'];
+$host=$_POST['host'];
+$tablename=$_POST['tablename'];
+$temp = $_POST['databasetype'];
 //
-//$username =$_POST['username'];
-//$database =$_POST['databasename'];
-//$password =$_POST['password'];
-//$host=$_POST['host'];
-//$tablename=$_POST['tablename'];
-//$temp = $_POST['databasetype'];
-//
-////A library I borrowed from a friend for connection
-//$atiaa = \ntentan\atiaa\Driver::getConnection(
-//    array(
-//        'driver' => $temp,
-//        'user' => $username,
-//        'dbname' => $database,
-//        'host'=> $host,
-//        'password'=>$password
-//    )
-//);
-////Make the query
-//
-//$createDatabase = $atiaa->query("Create database $database");
-//$createTable = $atiaa->query("Create table $tablename");
+//A library I borrowed from a friend for connection
+$atiaa = \ntentan\atiaa\Driver::getConnection(
+    array(
+        'driver' => $temp,
+        'user' => $username,
+        'dbname' => $database,
+        'host'=> $host,
+        'password'=>$password
+    )
+);
+//Make the query
+
+$createDatabase = $atiaa->query("Create database $database");
+$createTable = $atiaa->query("Create table $tablename");
 
 $connection = ssh2_connect('10.76.254.127', 22);
 
