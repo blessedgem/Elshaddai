27f7b91243ec3eba60e92c53bdbdc6461cfe7ffc
Nana Kwame <nanakwame123@gmail.com>
2016-05-01 05:34:21 +0000

    importTable changes

--- ImportTable.php
+++ ImportTable.php
@@ -2,32 +2,25 @@
 
 require "vendor/autoload.php";
 
-
-$databasename =$_POST['databasename'];
-$tablename =$_POST['tablename'];
-
-
-
-
-
-
+$databasename = $_POST['databasename'];
+$tablename = $_POST['tablename'];
 
 class ImportTable extends \ajumamoro\Job
 {
     private $tablename;
     private $databasename;
 
-    public function ExportDatabase()
+    public function ImportTable()
     {
-        file_put_contents("exportDatabase.sh", "ssh cloudera@$virtualhost 'sqoop import-all-tables --connect 'jdbc:$databasetype://$localhost:$portnumber/$databasename' --username=$username -P=$password -m 1' &> exportDatabse.out");
+        file_put_contents("ImportTable.sh", "ssh cloudera@$virtualhost 'sqoop import-all-tables --connect 'jdbc:$databasetype://$localhost:$portnumber/$databasename' --username=$username -P=$password -m 1' &> ImportTable.out");
     }
 
 
     public function go()
     {
-        file_put_contents("exportDatabase.sh", "ssh cloudera@{$this->getAttribute('virtualhost')} 'sqoop import-all-tables --connect 'jdbc:{$this->getAttribute('databasetype')}://{$this->getAttribute('localhost')}:{$this->getAttribute('portnumber')}/{$this->getAttribute('databasename')}' --username={$this->getAttribute('username')} --password={$this->getAttribute('password')} ' &> exportDatabse.out");
+        file_put_contents("ImportTable.sh", "ssh cloudera@{$this->getAttribute('virtualhost')} 'sqoop import-all-tables --connect 'jdbc:{$this->getAttribute('databasetype')}://{$this->getAttribute('localhost')}:{$this->getAttribute('portnumber')}/{$this->getAttribute('databasename')}' --username={$this->getAttribute('username')} --password={$this->getAttribute('password')} ' &> ImportTable.out");
             
         $this->log("Executing Job");
-        exec("bash exportDatabase.sh ");
+        exec("bash ImportTable.sh ");
     }
 }
